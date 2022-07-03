32d8d0f3d7471bd4cba20598147d71f166f772e3
Sidharth Sharma <54902978+sharmasidharth1910@users.noreply.github.com>
2022-07-04 00:03:07 +0530

    Added some new routes.

--- lib/router.dart
+++ lib/router.dart
@@ -6,7 +6,7 @@ Route<dynamic> generateRoute(RouteSettings routeSettings) {
     case AuthScreen.routeName:
       return MaterialPageRoute(
         settings: routeSettings,
-        builder: (_) => AuthScreen(),
+        builder: (_) => const AuthScreen(),
       );
     default:
       return MaterialPageRoute(
